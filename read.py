b50a535466891cd6d3562bf33675f7149ad6c5d4
noelkuo <bbmm0208@gmail.com>
2021-04-09 22:41:56 +0800

    add avg of reviews

--- read.py
+++ read.py
@@ -7,11 +7,13 @@ with open('reviews.txt', 'r') as f:
 		count += 1 # count = count +1
 		if count % 1000 == 0:  # %用來求餘數 1000的倍數
 		    print(len(data))
+		    
+### 計算留言的平均長度
 
+sum_len = 0
+for i in data:
+	sum_len = sum_len + len(i)
+	print(sum_len)
 
-print(len(data))
-
-# print(data)
-print(data[0]) 
-print('---------------------')
-print(data[1])
+print('檔案讀取完了，總共有', len(data), '筆資料')
+print('留言的平均長度是', sum_len/len(data))
