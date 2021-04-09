3d4135190552f7f33a808fee57eecda1099aefa1
noelkuo <bbmm0208@gmail.com>
2021-04-09 21:46:59 +0800

    list

---
+++ list.py
@@ -0,0 +1,14 @@
+
+
+a = ['Toyota', 'Honda']
+print(a)
+print(a[0])
+
+a.append('Audi') # append 是一種函式
+print(a[2])
+
+# list 長度
+print(len(a))
+
+print('Audi' in a) # 用in檢查是否有此東西 a是一個清單
+print('BMW' in a)
