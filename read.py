b35975f66c0b354ce11560f30783b84b1d583359
noelkuo <bbmm0208@gmail.com>
2021-04-09 22:55:28 +0800

    add filter function2

--- read.py
+++ read.py
@@ -15,15 +15,23 @@ for i in data:
 	sum_len = sum_len + len(i)
 	print(sum_len)
 
-print('檔案讀取完了，總共有', len(data), '筆資料')
+print('檔案讀取完了，總共有', len(data), '筆留言')
 print('留言的平均長度是', sum_len/len(data))
 
 # 查詢哪些留言小於100個字 -- 篩選
-
 new = []
 for j in data:
 	if len(j) < 100:
 		new.append(j)
 
-print('一共有', len(new), '筆資料長度小於100')
+print('一共有', len(new), '筆留言長度小於100')
 print(new[0])
+
+
+# 查詢哪些留言有包含 'good' -- 篩選
+good = []
+for k in data:
+	if 'good' in k:
+		good.append(k)
+
+print('一共有', len(good), '筆留言包含 good')
