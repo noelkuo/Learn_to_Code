369b11c5eaaac00d6659154d1151515b93fc5957
noelkuo <bbmm0208@gmail.com>
2021-04-10 21:40:41 +0800

    save data

--- products.py
+++ products.py
@@ -5,12 +5,7 @@ while True:
 	if name == 'q': # exit
 		break
 	price = input('請輸入商品價格: ')
-	# p = []
-	# p.append(name)
-	# p.append(price)
-
-	# p = [name, price]
-	# products.append(p)
+	price = int(price) # 轉成整數 非字串
 
 	products.append([name, price])
 
@@ -20,3 +15,10 @@ for p in products:
 	# print(p) # 個別清單
 	# print(p[0]) # 清單的商品名
 	print(p[0], '的價格是', p[1], '元') # 清單的商品名
+
+
+## 檔案儲存
+with open('products.csv', 'w', encoding='utf-8') as f: # 無解
+	f.write('商品,價格 \n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n') # str 將數字改成字串
