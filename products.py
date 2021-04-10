7248e62adaf70071b3a42fc522275d4782cf7f4a
noelkuo <bbmm0208@gmail.com>
2021-04-10 22:44:21 +0800

    add check data

--- products.py
+++ products.py
@@ -1,13 +1,20 @@
-# 讀取檔案
+import os # 作業系統模組 operating system
+
 products = []
 
-with open('products.csv', 'r') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue #繼續 ; break: 跳出
-		name, price = line.strip().split(',')   # split做切割，但記得還有換行的符號
-		products.append([name, price])
-print(products)
+if os.path.isfile('products.csv'): # 檢察檔案在不在
+	print('有檔案!')
+	# 讀取檔案
+	with open('products.csv', 'r') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue #繼續 ; break: 跳出
+			name, price = line.strip().split(',')   # split做切割，但記得還有換行的符號
+			products.append([name, price])
+	print(products)
+
+else:
+	print('找不到檔案.....')
 
 # 讓使用者輸入
 while True:
@@ -18,11 +25,10 @@ while True:
 	price = int(price) # 轉成整數 非字串
 
 	products.append([name, price])
+print(products)
 
 # 列印商品價格
 for p in products:
-	# print(p) # 個別清單
-	# print(p[0]) # 清單的商品名
 	print(p[0], '的價格是', p[1], '元') # 清單的商品名
 
 
