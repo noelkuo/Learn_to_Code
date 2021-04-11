3ec774826d928dc97a5e0232d6db700ca601541a
noelkuo <bbmm0208@gmail.com>
2021-04-11 16:41:03 +0800

    Line version

---
+++ chat2.py
@@ -0,0 +1,58 @@
+
+def read_file(filename):
+	lines = []
+	with open(filename, 'r', encoding='utf-8-sig') as f:
+		for line in f:
+			lines.append(line.strip())
+	return lines
+
+
+def convert(lines):
+	person = None # 無
+	allen_sticker_count = 0
+	viki_sticker_count = 0
+	allen_word_count = 0
+	viki_word_count = 0
+	allen_image_count = 0
+	viki_image_count = 0
+	for line in lines:
+		s = line.split(' ') # split 切割; 切割完會成為清單
+		time = s[0]
+		name = s[1]
+		if name == 'Allen':
+			if s[2] == '貼圖':
+				allen_sticker_count += 1
+			elif s[2] == '圖片':
+				allen_image_count += 1
+			else:
+				for m in s[2:]:
+					allen_word_count += len(m) # allen_word_count = allen_word_count + len(m)
+		elif name == 'Viki':
+			if s[2] == '貼圖':
+				viki_sticker_count += 1
+			elif s[2] == '圖片':
+				viki_image_count += 1
+			else:
+				for n in s[2:]:
+					viki_word_count += len(n)
+	print('Allen說了', allen_word_count, '個字')
+	print('Allen傳了', allen_sticker_count, '個貼圖以及', allen_image_count, '個圖片')
+
+	print('Viki說了', viki_word_count, '個字')
+	print('Viki傳了', viki_sticker_count, '個貼圖以及', viki_image_count, '個圖片')
+
+
+
+def write_file(filename, lines):
+	with open(filename, 'w') as f:
+		for line in lines:
+			f.write(line + '\n')
+
+
+def main():
+	lines = read_file('LINE-Viki.txt')
+	# print(lines)
+	lines = convert(lines)
+	# write_file('output.txt', lines)
+
+main()
